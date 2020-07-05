dec6baf274950e9320ca3f0e466968480c56df7f
msmfathih <msmfathih@users.noreply.github.com>
2020-07-05 12:31:14 +0400

    comments added for drag and drop functions and alert methods

--- Browser_Interaction/DragandDrop.py
+++ Browser_Interaction/DragandDrop.py
@@ -10,6 +10,9 @@ driver.implicitly_wait(10)
 driver.maximize_window()
 
 
+"""
+Types of Alert 
+"""
 
 ack = ActionChains(driver)
 ack.move_to_element(driver.find_element_by_xpath("//a[contains(text(),'SwitchTo')]")).perform()
@@ -36,54 +39,57 @@ time.sleep(3)
 driver.switch_to.alert.accept()
 driver.switch_to.alert.send_keys("fathih")
 
-time.sleep(3)
+"""
+-------------------**********************------------------------
+"""
 
-# ack = ActionChains(driver)
-# ack.click().perform()
-#
-#
-#
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box6']")
-# target_element = driver.find_element_by_xpath("//div[@id='box106']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box5']")
-# target_element = driver.find_element_by_xpath("//div[@id='box105']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box3']")
-# target_element = driver.find_element_by_xpath("//div[@id='box103']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box7']")
-# target_element = driver.find_element_by_xpath("//div[@id='box107']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box7']")
-# target_element = driver.find_element_by_xpath("//div[@id='box107']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box4']")
-# target_element = driver.find_element_by_xpath("//div[@id='box104']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box1']")
-# target_element = driver.find_element_by_xpath("//div[@id='box101']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-#
-#
-# source_element = driver.find_element_by_xpath("//div[@id='box2']")
-# target_element = driver.find_element_by_xpath("//div[@id='box102']")
-# action = ActionChains(driver)
-# action.drag_and_drop(source_element,target_element).perform()
-# driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+"""
+Handling the drag and drop
+"""
+
+ack = ActionChains(driver)
+ack.click().perform()
+
+source_element = driver.find_element_by_xpath("//div[@id='box6']")
+target_element = driver.find_element_by_xpath("//div[@id='box106']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+source_element = driver.find_element_by_xpath("//div[@id='box5']")
+target_element = driver.find_element_by_xpath("//div[@id='box105']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+source_element = driver.find_element_by_xpath("//div[@id='box3']")
+target_element = driver.find_element_by_xpath("//div[@id='box103']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+source_element = driver.find_element_by_xpath("//div[@id='box7']")
+target_element = driver.find_element_by_xpath("//div[@id='box107']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+source_element = driver.find_element_by_xpath("//div[@id='box7']")
+target_element = driver.find_element_by_xpath("//div[@id='box107']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+
+source_element = driver.find_element_by_xpath("//div[@id='box4']")
+target_element = driver.find_element_by_xpath("//div[@id='box104']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+
+source_element = driver.find_element_by_xpath("//div[@id='box1']")
+target_element = driver.find_element_by_xpath("//div[@id='box101']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+
+
+source_element = driver.find_element_by_xpath("//div[@id='box2']")
+target_element = driver.find_element_by_xpath("//div[@id='box102']")
+action = ActionChains(driver)
+action.drag_and_drop(source_element,target_element).perform()
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
