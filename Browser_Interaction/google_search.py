80f6e86c726600871c634b8a08016e3dd0aef1cb
msmfathih <msmfathih@users.noreply.github.com>
2020-07-10 22:37:05 +0400

    google search added

---
+++ Browser_Interaction/google_search.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.google.com/?gl=us&hl=en&pws=0")
+driver.implicitly_wait(10)
+
+driver.find_element(By.NAME, 'q').send_keys("Amazon")
+optionsList = driver.find_elements(By.CSS_SELECTOR, 'ul.erkvQe li span')
+print(len(optionsList))
+
+for ele in optionsList:
+    print(ele.text)
+    if ele.text == 'amazon prime':
+        ele.click()
+        break
+
+time.sleep(10)
+driver.quit()
+
