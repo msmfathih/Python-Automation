5294be6fdec171dbbe6a06d285be59a6ab08c9db
msmfathih <msmfathih@users.noreply.github.com>
2020-11-28 10:49:16 +0400

    date picker for loop

--- Browser_Interaction/datePicker.py
+++ Browser_Interaction/datePicker.py
@@ -6,22 +6,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 baseURL = "http://demo.automationtesting.in/Datepicker.html"
+driver.implicitly_wait(10)
 driver.get(baseURL)
 
-
-click_datapicker = driver.find_element(By.ID, 'datepicker1')
+click_datapicker = driver.find_element(By.ID, 'datepicker2')
 click_datapicker.click(),time.sleep(2)
 
-driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
 
-pick_date = driver.find_element(By.XPATH, "//a[contains(text(),'22')]")
-pick_date.click()
+# pick_date = driver.find_element(By.XPATH, "//a[contains(text(),'22')]")
+# pick_date.click()
+#
+# alldates = driver.find_elements(By.ID, "//table[@class='ui-datepicker-calendar']//a")
+
+alldates = driver.find_elements(By.ID, "//div[@class='datepick-popup']//a")
+for date_element in alldates:
+    date = date_element.text
+    print(date)
+    if date == '20':
+        date_element.click()
+        break
 
 
-driver.find_element(By.CSS_SELECTOR, "#datepicker2").send_keys("11/26/2020")
+
+
+
+# driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
+
+# pick_date = driver.find_element(By.XPATH, "//a[contains(text(),'22')]")
+# pick_date.click()
+
+# driver.find_element(By.CSS_SELECTOR, "#datepicker2").send_keys("11/26/2020")
 
 
 
